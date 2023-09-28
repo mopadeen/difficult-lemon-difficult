0fb3c149b8fb6003ef11b132e8dda8dfc5a66c0b
Andrea Hare <andreaylhare@gmail.com>
2023-09-28 18:37:37 +1000

    arrays

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-// Assignment code here
+// // Assignment code here
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+// // Get references to the #generate element
+// var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+var lowercaseChars = ['a' , 'b' ,'c' , 'd' , 'e' , 'f' , 'g' , 'h' , 'i' , 'j' , 'k' , 'l' , 'm' , 'n' , 'o' , 'p' , 'q' , 'r' , 's' , 't' , 'u' , 'v' , 'w' , 'x' , 'y' , 'z'];
+var uppercaseChars = ['A' , 'B' , 'C' , 'D' , 'E' , 'F' , 'G' , 'H' , 'I' , 'J' , 'K' , 'L' , 'M' , 'N' , 'O' , 'P' , 'Q' , 'R' , 'S' , 'T' , 'U' , 'V' , 'W' , 'X' , 'Y' , 'Z'];
+var numberChars = ['0' , '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9'];
+var specialChars = ['!' '@' '#' '$' '%' '^' '&' '*' '(' ''' ')' '_' '+' '{' '}' '[' ']' '|' ':' ';' '"' '<' '>' ',' '.' '?' '/' '''];
+
+function getPasswordOptions() {
+  
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
